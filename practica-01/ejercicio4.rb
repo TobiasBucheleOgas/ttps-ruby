a6d2e9b83dde2c834e7aec7d73fa42d9456515ea
tobiasbucheleogas <tobiasbucheleogas@gmail.com>
2025-10-23 13:45:34 +0000

    Ejercicio 4 - Comparacion de variables

---
+++ practica-01/ejercicio4.rb
@@ -0,0 +1,11 @@
+#Dos String con la misma cadena pero creados por separados = false
+puts 'TTPS Ruby'.object_id == 'TTPS Ruby'.object_id
+
+#Asignando una variable
+texto = 'TTPS Ruby'
+puts texto.object_id == texto.object_id
+
+#Comparacion de contenido
+puts 'TTPS Ruby' == 'TTPS Ruby'
+
+
