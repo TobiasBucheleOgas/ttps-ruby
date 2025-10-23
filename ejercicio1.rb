e86cc8da6da1069ba936b7d42f4d46c6d80eb966
tobiasbucheleogas <tobiasbucheleogas@gmail.com>
2025-10-23 13:12:27 +0000

    Ejercicio1- Tipos basicos en Ruby

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -1,2 +1,23 @@
-puts "Hola desde Ruby"
+#Simbolo
+simbolo = :mi_simbolo
 
+#String
+cadena1 = "Hola Mundo"
+cadena2 = String.new("Hola Mundo")
+
+#Arreglo
+arreglo1 = [1, 2, 3]
+arreglo2 = Array.new([1, 2, 3])
+
+#Hash
+hash1 = {clave: "valor", edad:25}
+hash2 = Hash.new({clave: "valor", edad:25})
+
+#Para verificar los tipos
+puts simbolo.class 
+puts cadena1.class
+puts cadena2.class
+puts arreglo1.class
+puts arreglo2.class
+puts hash1.class
+puts hash2.class
